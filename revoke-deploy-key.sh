215e5372c484d7b75ff14e2d0570d89c0c8afaaa
DavideTriso <davidetrisolini@yahoo.it>
2023-02-10 19:19:08 +0000

    Fix: - change sed delimiter to avoid problems with slashes - remove keys from the passed dir

--- revoke-deploy-key.sh
+++ revoke-deploy-key.sh
@@ -3,9 +3,10 @@
 repository_name=$1
 dir=$2
 
-rm ~/.ssh/"$repository_name"_deploy-key
-rm ~/.ssh/"$repository_name"_deploy-key.pub
+rm "$dir"/"$repository_name"_deploy-key
+rm "$dir"/"$repository_name"_deploy-key.pub
 
-sed -z "s/\n\nHost ${repository_name}\n  Hostname github.com\n  User git\n  IdentityFile ${dir}/${repository_name}_deploy-key//g" -i "$dir"/config
+sed -z "s|Host ${repository_name}\n  Hostname github.com\n  User git\n  IdentityFile ${dir}/${repository_name}_deploy-key\n  IdentitiesOnly yes||g" -i "$dir"/config
+sed -z "s|\n\n\n|\n|g" -i "$dir"/config
 
 echo The deploy key has been revoked.
