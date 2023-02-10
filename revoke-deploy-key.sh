eb91017261d7e5c86555d8c0be714d2f77174c93
Davide Trisolini <davidetrisolini@yahoo.it>
2023-02-10 17:38:32 +0100

    Update revoke-deploy-key.sh

--- revoke-deploy-key.sh
+++ revoke-deploy-key.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 repository_name=$1
+dir=$2
 
 rm ~/.ssh/"$repository_name"_deploy-key
 rm ~/.ssh/"$repository_name"_deploy-key.pub
 
-sed -z "s/\n\nHost ${repository_name}\n  Hostname github.com\n  User git\n  AddKeysToAgent yes\n  UseKeychain yes\n  IdentityFile ~\/.ssh\/${repository_name}_deploy-key//g" -i ~/.ssh/config
+sed -z "s/\n\nHost ${repository_name}\n  Hostname github.com\n  User git\n  IdentityFile ${dir}/${repository_name}_deploy-key//g" -i "$dir"/config
 
 echo The deploy key has been revoked.
