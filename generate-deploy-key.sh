37af89d7239bbed3994e6f4bf621bc62db5e7b63
Davide Trisolini <davidetrisolini@yahoo.it>
2023-02-10 17:33:53 +0100

    Update generate-deploy-key.sh

--- generate-deploy-key.sh
+++ generate-deploy-key.sh
@@ -2,15 +2,16 @@
 
 repository_name=$1
 email_address=$2
+dir=$3
 
-ssh-keygen -t ed25519 -f ~/.ssh/"$repository_name"_deploy-key -N "" -C $email_address
+mkdir -p $dir
+touch -a "$dir"/config
 
-mkdir -p ~/.ssh
-touch -a ~/.ssh/config
+ssh-keygen -t ed25519 -f "$dir"/"$repository_name"_deploy-key -N "" -C $email_address
 
-printf "\n\nHost %s\n  Hostname github.com\n  User git\n  AddKeysToAgent yes\n  UseKeychain yes\n  IdentityFile ~/.ssh/%s_deploy-key" $repository_name $repository_name >> ~/.ssh/config
+printf "\n\nHost %s\n  Hostname github.com\n  User git\n  IdentityFile %s/%s_deploy-key\n   IdentitiesOnly yes\n" $repository_name $dir $repository_name >> "$dir"/config
 
 echo "======================================================="
 echo "Done! The public deploy key for your repository is:"
-echo $(cat ~/.ssh/"$repository_name"_deploy-key.pub)
+echo $(cat "$dir"/"$repository_name"_deploy-key.pub)
 echo "======================================================="
