c52cb2bdd80849589a43ac1b51c5ef2713036901
Davide Trisolini <davidetrisolini@yahoo.it>
2023-02-10 17:36:24 +0100

    Update generate-deploy-key.sh

--- generate-deploy-key.sh
+++ generate-deploy-key.sh
@@ -9,7 +9,7 @@ touch -a "$dir"/config
 
 ssh-keygen -t ed25519 -f "$dir"/"$repository_name"_deploy-key -N "" -C $email_address
 
-printf "\n\nHost %s\n  Hostname github.com\n  User git\n  IdentityFile %s/%s_deploy-key\n   IdentitiesOnly yes\n" $repository_name $dir $repository_name >> "$dir"/config
+printf "\n\nHost %s\n  Hostname github.com\n  User git\n  IdentityFile %s/%s_deploy-key\n  IdentitiesOnly yes\n" $repository_name $dir $repository_name >> "$dir"/config
 
 echo "======================================================="
 echo "Done! The public deploy key for your repository is:"
